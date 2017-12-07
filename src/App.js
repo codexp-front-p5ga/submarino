a878d7f943d1f1a1aa5b93584edf6fce80d6bc36
MICHEL CALHEIROS <47302726841@jpmorgan.sp>
2017-12-07 19:41:20 -0200

    Initial project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Example from './components/Example/Example';
+
+class App extends Component {
+  render() {
+    return (
+      <Example text="Component"/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Example/Example.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Example.css';
+
+export default () => (
+    <p className="example">Example</p>
+);
