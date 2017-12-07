7355951493887f768a42818f35c279373b73b3b4
REBECCA ARANTES FERREIRA <44493175800@jpmorgan.sp>
2017-12-07 21:39:11 -0200

    mapa.js com html semantico e app.js em andamento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Example from './components/Example/Example';
+var mapa1 = [
+  {
+    titulo: 'Institucional',
+    subtitulo1: 'Garantia Estendida',
+    subtitulo2: 'Relações com Investidores',
+    subtitulo3: 'Trabalhe Conosco',
+    subtitulo4: 'Política de Privacidade',
+    subtitulo5: 'Companhia Verde',
+    subtitulo6: 'Assessoria de Imprensa',
+    subtitulo7: 'Venda no Submarino',
+    subtitulo8: 'Serviços de Entrega - Direct'
+  }];
 
-class App extends Component {
-  render() {
-    return (
-      <Example text="Component"/>
-    );
-  }
-}
+  var mapa2 = [
+    {
+    titulo2: 'Dúvidas',
+    subtitulo11: 'Atendimento',
+    subtitulo12: 'Trocas e Devoluções',
+    subtitulo13: 'Direito de Arrepedimento',
+    subtitulo14: 'Contato - Principais fornecedores',
+    subtitulo15: 'Política de Entrega',
+    subtitulo16: 'Disponibilidade de Produto',
+    subtitulo17: 'Alerta de e-mails e sites falsos',
+    subtitulo18: 'Regras do Site',
+    subtitulo19: 'Termos e Conduições de compra e venda de produtos',
+    subtitulo20: 'Procon-RJ'
+  }];
+
+  var mapa3 = [
+    {
+    titulo3: 'Cartão Submarino',
+    subtitulo21: 'Peça já o seu cartão',
+    subtitulo22: 'Central de Promoções',
+    subtitulo23: 'Programas Léguas',
+    subtitulo24: 'Melhor parcelamento no site',
+    subtitulo25: 'Limite'
+  }];
+
+var mapa4 = [
+  {
+    titulo4: 'Submarino Viagens',
+    subtitulo26: 'Promoções',
+    subtitulo27: 'Passagens Aéreas',
+    subtitulo28: 'Hotéis',
+    subtitulo29: 'Pacotes Turísticos',
+    subtitulo30: 'Ingressos',
+    subtitulo31: 'Cruzeiros marítimos',
+    subtitulo33: 'Resorts'
+  }];
+
+  var mapa5 = [{
+    titulo5: 'Submarino Prime',
+    subtitulo34: 'Seja Prime',
+    subtitulo35: 'Produtos Prime',
+    subtitulo36: 'Dúvidas',
+    subtitulo37: 'Termos e Condições'
+  }];
+
 
 export default App;
---
+++ src/components/Mapa/Mapa.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Mapa.css";
+
+
+export default (props) => (
+    <section className="mapa-site">
+        <h3 className="mapa-site_secao">{props.name}</h3>
+        <h4 className="mapa-site_expandir">></h4>
+        <h4 className="mapa-site_opcao">
+        <hr>   
+    </section>
+);
+
